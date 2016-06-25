28dcc45c909a5781243fb2311c5da501779f9120
JumpeiArai <ja.fileonly@gmail.com>
2016-06-26 02:31:37 +0900

    commit

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ io.on('connection', function (socket) {
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("MAPS server listening at", addr.address + ":" + addr.port);
+  console.log("App server listening at", addr.address + ":" + addr.port);
 });
