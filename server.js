4f4720615309d39e3f36ee367920718a586b3fad
s-higuma <bump.sunbear@gmail.com>
2016-06-26 08:09:12 +0900

    fixed miss push

--- server.js
+++ server.js
@@ -35,19 +35,24 @@ io.on('connection', function (socket) {
     io.socket.emit('startGame', users);
   });
   
-  
   //Disconncting action
   socket.on('disconnect',function(){
-    var id = socket.id.substring(2);
     console.log(users);
-    //Send socket "ID" which was disconnected for Clients
-    io.sockets.emit('removeuserStoC', id);
+    var id = searchIndex(socket.id.substring(2));
     if(typeof users[id] !== "undefined"){
       console.log(users[id].name + " leave room");
     }
-    console.log("ID: " + id + " has disconnected");
-    delete users[id];
+    console.log("ID: " + socket.id.substring(2) + " has disconnected");
+    users.splice(id, 1);
+    console.log(users);
+    io.sockets.emit('disconnect', users);
   });
+
+  function searchIndex(socketID){
+    for(var i=0; i < users.length; i++){
+      if(users[i].id == socketID) return i; 
+    }
+  }
   
 });
 
