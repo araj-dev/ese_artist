21b01b0cae2f0426c72d4abccfdc6b8d1307b5d1
s-higuma <bump.sunbear@gmail.com>
2016-06-26 04:40:56 +0900

    error

--- server.js
+++ server.js
@@ -38,11 +38,12 @@ io.on('connection', function (socket) {
   //Disconncting action
   socket.on('disconnect',function(){
     var id = socket.id.substring(2);
-    delete users[id];
     //Send socket "ID" which was disconnected for Clients
-    io.sockets.emit('removeuserStoC',socket.id.substring(2));
-    console.log("ID: "+id+" has disconnected");
-    console.log(users);
+    io.sockets.emit('removeuserStoC', id);
+    var user = users[id];
+    console.log(user.name + " leave room");
+    console.log("ID: " + id + " has disconnected");
+    delete users[id];
   });
   
 });
